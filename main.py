1be4aa58bf9c53020a86f8347a0d626cafca263b
Pedro <pedrolaraburu@gmail.com>
2023-06-03 10:53:21 -0300

    add exercise 4

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from exercises.ex1 import fibonnaci
 from exercises.ex2 import isPalindrome
 from exercises.ex3 import isPrime
+from exercises.ex4 import sortingAlgorithm
 if __name__ == '__main__':
+    # Fibonnaci
     print(fibonnaci(7))
 
+    # Is Palindrome
     t1 = isPalindrome('nurses run')
     print(t1)
 
@@ -13,4 +16,10 @@ if __name__ == '__main__':
     t3 = isPalindrome(121.121)
     print(t3)
 
+    # Is Prime
     print(isPrime(17))
+
+    # Sorting Algorithm
+    arr = [8, 2, 7, 3, 5, 12]
+    sortingAlgorithm(arr)
+    print(arr)
